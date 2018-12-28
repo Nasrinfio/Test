fac5684e9923ceda5cbc714ad8704d9ad9be238e
Nasrinfio <nasrinrilwana@gmail.com>
2018-12-28 10:26:07 +0530

    new method scorpio added

--- src/org/car/OldCars.java
+++ src/org/car/OldCars.java
@@ -2,18 +2,28 @@ package org.car;
 
 public class OldCars {
 
+	public static void scorpio() {
+		System.out.println("new model");
+		System.out.println("new model");
+		
+		System.out.println("new model");
+	}
 	public static void maruti800() {
 		System.out.println("gives good mileage");
+		System.out.println("new model");
 
 	}
 	
 	public static void zen() {
 		System.out.println("latest version");
 
+		System.out.println("new model");
 	}
 	public static void main(String[] args) {
+		scorpio();
 		maruti800();
 		zen();
+		
 	}
 	
 }
