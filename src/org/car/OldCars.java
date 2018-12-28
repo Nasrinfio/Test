0b175a608e29976d6f6a98989ca8c9842eb67419
Nasrinfio <nasrinrilwana@gmail.com>
2018-12-28 10:06:29 +0530

    created car program

---
+++ src/org/car/OldCars.java
@@ -0,0 +1,13 @@
+package org.car;
+
+public class OldCars {
+
+	public static void maruti800() {
+		System.out.println("gives good mileage");
+
+	}
+	public static void main(String[] args) {
+		maruti800();
+	}
+	
+}
