f8a9bc4dc3885c18f642c1c3cf0468d5462dd2e2
Nasrinfio <nasrinrilwana@gmail.com>
2018-12-28 10:41:47 +0530

    new method ritz

--- src/org/car/OldCars.java
+++ src/org/car/OldCars.java
@@ -1,24 +1,31 @@
 package org.car;
 
 public class OldCars {
+	
+	public static void ritz() {
+		System.out.println("flop model");
+
+	}
 
 	public static void innova() {
 		System.out.println("new car");
+		System.out.println("flop model");
 
 	}
 	public static void maruti800() {
 		System.out.println("gives good mileage");
-
+		System.out.println("flop model");
 	}
 	
 	public static void zen() {
 		System.out.println("latest version");
-
+		System.out.println("flop model");
 	}
 	public static void main(String[] args) {
 		maruti800();
 		zen();
 		innova();
+		ritz();
 	}
 	
 }
